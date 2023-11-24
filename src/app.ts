c8e06ad3a0235675e123f898d9d84dfb5bb41071
lsprgabriel <ogabrielpereira@pm.me>
2023-11-24 13:21:19 -0300

    feat: Implementação das features do GPT

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { chatCompletion } from './services/openai'
+import { qea, translate, summarize, getKeywords } from './services/openai'
 import { Router, Request, Response } from 'express';
 
 const app = express();
@@ -12,16 +12,49 @@ route.get('/', (req: Request, res: Response) => {
     res.json({ message: 'Hello world with Typescript' })
 })
 
-route.post('/chat', async (req: Request, res: Response) => {
+route.post('/duck/qea', async (req: Request, res: Response) => {
     const { prompt } = req.body
 
     try {
-        const response = await chatCompletion(prompt)
+        const response = await qea(prompt)
         res.json({ response })
     } catch (error) {
         res.status(500).json({ error })
     }
 })
+
+route.post('/duck/translate', async (req: Request, res: Response) => {
+    const { prompt, from, to } = req.body
+
+    try {
+        const response = await translate(prompt, from, to)
+        res.json({ response })
+    } catch (error) {
+        res.status(500).json({ error })
+    }
+});
+
+route.post('/duck/sumup', async (req: Request, res: Response) => {
+    const { prompt } = req.body
+
+    try {
+        const response = await summarize(prompt)
+        res.json({ response })
+    } catch (error) {
+        res.status(500).json({ error })
+    }
+});
+
+route.post('/duck/keys', async (req: Request, res: Response) => {
+    const { prompt } = req.body
+
+    try {
+        const response = await getKeywords(prompt)
+        res.json({ response })
+    } catch (error) {
+        res.status(500).json({ error })
+    }
+});
 
 app.use(route)
 
--- src/services/openai.ts
+++ src/services/openai.ts
@@ -9,9 +9,36 @@ const openai = new OpenAI({
     apiKey: OPENAI_API_KEY,
 });
 
-export const chatCompletion = async (prompt: string) => {
+export const qea = async (prompt: string) => {
     const completion = await openai.chat.completions.create({
         messages: [{ role: 'user', content: prompt }],
+        model: 'gpt-3.5-turbo',
+        temperature: 0.7,
+    });
+    return completion
+}
+
+export const translate = async (prompt: string, from: string, to: string) => {
+    const completion = await openai.chat.completions.create({
+        messages: [{ role: 'user', content: `Translate ${prompt} from ${from} to ${to}` }],
+        model: 'gpt-3.5-turbo',
+        temperature: 0.7,
+    });
+    return completion
+}
+
+export const summarize = async (prompt: string) => {
+    const completion = await openai.chat.completions.create({
+        messages: [{ role: 'user', content: `Summarize ${prompt}` }],
+        model: 'gpt-3.5-turbo',
+        temperature: 0.7,
+    });
+    return completion
+}
+
+export const getKeywords = async (prompt: string) => {
+    const completion = await openai.chat.completions.create({
+        messages: [{ role: 'user', content: `Get 7 keywords from ${prompt}` }],
         model: 'gpt-3.5-turbo',
         temperature: 0.7,
     });
