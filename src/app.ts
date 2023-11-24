5153d15ddb9ef01d2070b28980bcd4708b9aeace
lsprgabriel <ogabrielpereira@pm.me>
2023-11-24 11:53:00 -0300

    openai service updated

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { chatCompletion } from './services/openai'
 import { Router, Request, Response } from 'express';
 
 const app = express();
@@ -9,6 +10,17 @@ app.use(express.json())
 
 route.get('/', (req: Request, res: Response) => {
     res.json({ message: 'Hello world with Typescript' })
+})
+
+route.post('/chat', async (req: Request, res: Response) => {
+    const { prompt } = req.body
+
+    try {
+        const response = await chatCompletion(prompt)
+        res.json({ response })
+    } catch (error) {
+        res.status(500).json({ error })
+    }
 })
 
 app.use(route)
--- src/services/openai.ts
+++ src/services/openai.ts
@@ -1,1 +1,19 @@
-console.log("Hello from openai.ts")
+import OpenAI from 'openai';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+const openai = new OpenAI({
+    apiKey: OPENAI_API_KEY,
+});
+
+export const chatCompletion = async (prompt: string) => {
+    const completion = await openai.chat.completions.create({
+        messages: [{ role: 'user', content: prompt }],
+        model: 'gpt-3.5-turbo',
+        temperature: 0.7,
+    });
+    return completion
+}
